b49fdd69bf9263e4d4a7e70f7e2e7db53d25882c
tsukasa inoue <inoue.tsukasa.4@gmail.com>
2024-07-30 14:51:30 +0900

    update sample test

--- src/cli/main.go
+++ src/cli/main.go
@@ -5,3 +5,10 @@ import "fmt"
 func main() {
 	fmt.Println("sample app")
 }
+
+func SampleMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
--- src/cli/main_test.go
+++ src/cli/main_test.go
@@ -1,5 +1,29 @@
-package main_test
+package main
 
 import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 	_ "github.com/stretchr/testify/assert"
 )
+
+func TestSampleMax(t *testing.T) {
+	tests := map[string]struct {
+		a    int
+		b    int
+		want int
+	}{
+		"1 and 2": {
+			a:    1,
+			b:    2,
+			want: 2,
+		},
+	}
+
+	for title, tt := range tests {
+		t.Run(title, func(t *testing.T) {
+			res := SampleMax(tt.a, tt.b)
+			assert.Equal(t, tt.want, res)
+		})
+	}
+}
